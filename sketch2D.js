a8e5c19b1eae6f26d8ce38caa526f52cf90992b3
Arjit Mehta <arjit.mehta@enumeracy.org>
2025-02-11 18:42:21 +0530

    Initial commit

---
+++ sketch2D.js
@@ -0,0 +1,80 @@
+/*
+ * 👋 Hello! This is an ml5.js example made and shared with ❤️.
+ * Learn more about the ml5.js project: https://ml5js.org/
+ * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
+ *
+ * This example demonstrates drawing skeletons on poses for the MoveNet model.
+ */
+
+let video;
+let bodyPose;
+let poses = [];
+let connections;
+
+function preload() {
+  // Load the bodyPose model
+  bodyPose = ml5.bodyPose("BlazePose");
+}
+
+// Callback function for when bodyPose outputs data
+function gotPoses(results) {
+    // Save the output to the poses variable
+    poses = results;
+  }
+
+function setup() {
+  // Set canvas size to match the window size
+  console.log('size',windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+  
+
+  // Create the video and hide it
+  video = createCapture(VIDEO);
+  video.size(windowWidth, windowHeight);
+  video.hide();
+
+  // Start detecting poses in the webcam video
+  bodyPose.detectStart(video, gotPoses);
+  // Get the skeleton connection information
+  connections = bodyPose.getSkeleton();
+}
+
+function draw() {
+  // Draw the webcam video
+  image(video, 0, 0, width, height);
+
+  // Draw the skeleton connections
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i];
+    for (let j = 0; j < connections.length; j++) {
+      let pointAIndex = connections[j][0];
+      let pointBIndex = connections[j][1];
+      let pointA = pose.keypoints[pointAIndex];
+      let pointB = pose.keypoints[pointBIndex];
+      // Only draw a line if both points are confident enough
+      if (pointA.confidence > 0.1 && pointB.confidence > 0.1) {
+        stroke(255, 0, 0);
+        strokeWeight(2);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+      }
+    }
+  }
+
+  // Draw all the tracked landmark points
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i];
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      let keypoint = pose.keypoints[j];
+      // Only draw a circle if the keypoint's confidence is bigger than 0.1
+      if (keypoint.confidence > 0.1) {
+        fill(0, 0, 255);
+        noStroke();
+        circle(keypoint.x, keypoint.y, 10);
+      }
+    }
+  }
+}
+function mousePressed() {
+    console.log(poses);
+  }
+
